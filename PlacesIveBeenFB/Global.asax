8abbb21f18d15bb0427860eb20bbaa5ed387d13a
ikhanage <quiteimran@yahoo.co.uk>
2016-01-21 22:52:17 +0000

    Initial commit for the Places I've Been Facebook map.

---
+++ PlacesIveBeenFB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlacesIveBeenFB.MvcApplication" Language="C#" %>
---
+++ PlacesIveBeenFB/Models/FacebookPicture.cs
@@ -0,0 +1,7 @@
+﻿namespace PlacesIveBeenFB.Models
+{
+    public class FacebookPicture
+    {
+        public string Url { get; set; }
+    }
+}
